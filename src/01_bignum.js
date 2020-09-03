ab9808a3242b04c33df7fb7503319a4de2af6dd9
adrianbparra <adrianplane12@gmail.com>
2020-09-02 19:43:30 -0700

    completed more work

---
+++ src/01_bignum.js
@@ -0,0 +1,8 @@
+// Print out 2 to the 65536 power
+// (try doing the same thing in the JS console and see what it outputs)
+
+// YOUR CODE HERE
+console.log(Math.pow(2,65536))
+
+// returns Infinity
+
